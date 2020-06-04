4dce3f28eacdf67e8f1eb5df7a4fd998890b5f10
Casido1 <42690614+Casido1@users.noreply.github.com>
2020-06-04 15:31:10 +0100

    Create UgochukwuAnunihu.py

---
+++ scripts/UgochukwuAnunihu.py
@@ -0,0 +1,6 @@
+my_name = 'Ugochukwu Anunihu'
+my_email = 'ugochukwu.anunihu@gmail.com'
+hng_id = 'HNG-06503'
+language = 'python'
+
+print(f'Hello world, this is {my_name} with HNGi7 ID {hng_id} using {language} for stage 2 task. {my_email}')
